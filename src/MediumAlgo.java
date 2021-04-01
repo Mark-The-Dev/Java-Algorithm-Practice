8eec243e0ed96aa0f60a74afd57b72aeb237e971
Mark <SwiftTactics@gmail.com>
2021-04-01 18:36:10 -0400

    added a new algorithm

--- src/MediumAlgo.java
+++ src/MediumAlgo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MediumAlgo {
@@ -138,6 +139,29 @@ public class MediumAlgo {
             }
         }
         return output;
+    }
+
+    // Algorithm to find the minimum number of coins needed to hit n if possible.
+    public static int minNumberOfCoinsForChange(int n, int[] denoms) {
+
+        int[] numOfCoins = new int[n + 1];
+        Arrays.fill(numOfCoins, Integer.MAX_VALUE);
+        numOfCoins[0] = 0;
+        int toCompare = 0;
+
+        for (int denom : denoms){
+            for (int amount = 0; amount < numOfCoins.length; amount++){
+                if (denom <= amount){
+                    if(numOfCoins[amount - denom] == Integer.MAX_VALUE){
+                        toCompare = numOfCoins[amount - denom];
+                    } else{
+                        toCompare = numOfCoins[amount - denom] + 1;
+                    }
+                    numOfCoins[amount] = Math.min(numOfCoins[amount], toCompare);
+                }
+            }
+        }
+        return numOfCoins[n] != Integer.MAX_VALUE ? numOfCoins[n] : -1;
     }
 }
 
